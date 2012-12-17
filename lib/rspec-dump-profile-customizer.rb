3b82670f742239ee19970af54118d04b5434171e
Paul Schuegraf <paul@verticallabs.ca>
2012-12-17 11:36:29 -0800

    use Rspec config instead of Rails config

--- lib/rspec-dump-profile-customizer.rb
+++ lib/rspec-dump-profile-customizer.rb
@@ -6,16 +6,17 @@ module RSpecDumpProfileCustomizer
     require 'rspec/core/formatters/documentation_formatter'
 
     class Railtie < ::Rails::Railtie
-
-      config.rspec_dump_profile = ActiveSupport::OrderedOptions.new
-
       config.after_initialize do
-        RSpec::Core::Formatters::BaseTextFormatter.class_exec(config.rspec_dump_profile.number, config.rspec_dump_profile.second) do |number, second|
+        RSpec.configure do |config|
+          c.add_settings :dump_profile_number, :default => 15
+          c.add_settings :dump_profile_second, :default => 3.5
+        end
 
+        RSpec::Core::Formatters::BaseTextFormatter.class_exec do
           define_method("dump_profile") do
-            if number
+            if RSpec.configuration.dump_profile_number
               sorted_examples_for_number = examples.sort_by {|example| example.execution_result[:run_time] }.
-                reverse.first(number)
+                reverse.first(RSpec.configuration.dump_profile_number)
               total, slows = [examples, sorted_examples_for_number].map {|exs|
                 exs.inject(0.0) {|i, e| i + e.execution_result[:run_time] }}
               time_taken = slows / total
@@ -26,10 +27,11 @@ module RSpecDumpProfileCustomizer
                 output.puts "    #{yellow(format_seconds(example.execution_result[:run_time]))} #{yellow("seconds")} #{format_caller(example.location)}"
               end
             end
-            if second
-              output.puts cyan("\nSlow examples (over #{second} seconds):\n")
+
+            if RSpec.configuration.dump_profile_second
+              output.puts cyan("\nSlow examples (over #{RSpec.configuration.dump_profile_second} seconds):\n")
               sorted_examples_for_number = examples.sort_by { |example| example.execution_result[:run_time] }.
-                reverse.select { |example| example.execution_result[:run_time] > second.to_f }
+                reverse.select { |example| example.execution_result[:run_time] > RSpec.configuration.dump_profile_second.to_f }
               sorted_examples_for_number.each do |example|
                 output.puts "  #{example.full_description}"
                 output.puts "    #{yellow(format_seconds(example.execution_result[:run_time]))} #{yellow("seconds")} #{format_caller(example.location)}"
